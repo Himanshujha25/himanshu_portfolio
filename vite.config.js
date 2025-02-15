28b7f4c6dd8b76de799b0e7a4ea9fe1a2b6683ff
himanshu jha <jhahiamsnhu930@gmail.com>
2025-02-15 12:26:40 +0530

    last commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,  // Allows external access
+    strictPort: true,
+    port: 5173, // Default Vite port
+    allowedHosts: 'all'  // Allows all hosts
+  }
+});
