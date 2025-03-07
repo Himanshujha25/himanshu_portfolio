d8c96b5b05727afab9c2a6bc17afd8a050130120
himanshu jha <jhahiamsnhu930@gmail.com>
2025-03-07 23:23:20 +0530

    go

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+ // Make sure this import is correct
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
   server: {
-    host: true,  // Allows external access
+    host: true,
     strictPort: true,
-    port: 5173, // Default Vite port
-    allowedHosts: [
-      "5173-himanshujha-himanshupor-5p034hninja.ws-us117.gitpod.io",
-      ".gitpod.io"
-    ]  // Allows all hosts
-  }
+    port: 5173,
+  },
 });
