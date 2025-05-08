aa46113f9767ed2a9aa30084680d342a86f2df54
Himanshujha <166629497+Himanshujha25@users.noreply.github.com>
2025-05-08 22:11:03 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -45,7 +45,7 @@ import ecommerceIcon from "../assets/company/E-Commerce Platform.webp"
       id:"resume",
       title:"Resume",
       external:"true",
-      URL:"https://drive.google.com/file/d/1jPjhwdEp-hPIkP-WN7aIjw0DkAgBCQcW/view?usp=drive_link"
+      URL:"https://drive.google.com/file/d/1hO-qErdenmPtS3Afm-I434H0_zCg_gg1/view?usp=drive_link"
     },
   ];
   
